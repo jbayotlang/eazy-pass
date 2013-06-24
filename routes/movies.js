5bafcfba64ab454b45d6769e6be25443c4bb7d89
Jhudiel <jhudiel.bayotlang@outlook.ph>
2013-06-24 01:24:54 -0700

    First commit

---
+++ routes/movies.js
@@ -0,0 +1,31 @@
+/**
+* Sample Data
+*
+**/
+
+var movies = [
+ {
+ 	id: 1,
+ 	poster: "http://1.bp.blogspot.com/-sRraJu_vlPE/UYRFSMIxanI/AAAAAAAAAVU/pseCMLaEwPA/s1600/iron-man-3-poster.jpg"
+ },
+ {	
+ 	id: 2,
+ 	poster: "http://collider.com/wp-content/uploads/star-trek-2-into-darkness-poster.jpg"
+ },
+ {
+ 	id: 3,
+ 	poster: "http://1.bp.blogspot.com/-FhFiLWFPkZg/UV4WiY4y8cI/AAAAAAAAAPc/hewzpLquT40/s1600/ff6-new-movie-poster.jpg"
+ },
+ {
+ 	id: 4,
+ 	poster: "http://1.bp.blogspot.com/-yKOBq1-ASZI/UUG_X55Da0I/AAAAAAAAAG8/cQL1PZli8bE/s400/after-earth-ugly-poster.jpg"
+ }
+
+]
+
+
+
+
+exports.movies = function(req, res) {
+	res.json(movies);
+}
